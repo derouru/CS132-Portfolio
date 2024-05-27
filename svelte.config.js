b0f07a4210778cbe931eb3740e689b7882148484
Derouru <derouru@gmail.com>
2024-05-27 21:08:22 +0800

    Fix deployment config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+ // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+ // for more information about preprocessors
+ preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		//paths: {
-		//	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		//}
-	}
-
+ kit: {
+  adapter: adapter(),
+ }
 };
+
 export default config;
