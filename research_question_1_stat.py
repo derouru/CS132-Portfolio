4e77cedf7263908fa41388cc6d89a8e4092136a3
Audrey Dela Vega <ardelavega@up.edu.ph>
2024-05-28 09:29:46 +0800

    graphs and writeup for research question 1

--- research_question_1_stat.py
+++ research_question_1_stat.py
@@ -205,12 +205,14 @@ for i in range(10):
 for i in range(22):
     ratio = c16_women[i]/(c16_men[i]+c16_women[i]) if (c16_men[i]+c16_women[i]) > 0 else 0.00 
     total = (c16_men[i]+c16_women[i])
-    print("Business type ", i, ". M = ", c16_men[i], " W = ", c16_women[i],  " | Ratio: ", ratio, " | N = ", total)
+    print(i, ",M,", c16_men[i])
+    print(i, ",F,", c16_women[i]) 
 
 for i in range(7):
     ratio = c23_women[i]/(c23_men[i]+c23_women[i]) if (c23_men[i]+c23_women[i]) > 0 else 0.00
     total = (c23_men[i]+c23_women[i])
-    print("Worker type ", i,". M = ", c23_men[i], " W = ", c23_women[i],  " | Ratio: ", ratio, " | N = ", total)
+    print(i, ",M,", c23_men[i])
+    print(i, ",F,", c23_women[i]) 
 
 for i in range(39):
     match significant[i]:
