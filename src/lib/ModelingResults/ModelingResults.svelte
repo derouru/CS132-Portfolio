a7b046de1607cc952df627a88885208955640a68
Derouru <derouru@gmail.com>
2024-06-19 23:04:16 +0800

    Update text content for Modeling&Results

--- src/lib/ModelingResults/ModelingResults.svelte
+++ src/lib/ModelingResults/ModelingResults.svelte
@@ -8,45 +8,74 @@
 
 <html lang='en'>
     <h3 class="text-3xl py-4 font-medium">Identifying female-centric work types</h3>
-    <p class="text-left">The following are visualizations of the distribution of men and women in different work classifications:</p>
-    <!--INSERT 3 GRAPHS-->
+    <p class="text-left">The following are visualizations of the gender gap calculated for each category under different primary occupation types, different industry types, and worker class:</p>
+    <!--INSERT 3 GRAPHS HERE-->
     <span class="flex justify-center items-center">
         <!--<img class="mt-5" id="formula" src="{}" alt="formula" width="75%" height="auto">-->
     </span>
 
     <p class="text-left">The t-tests were able to identify that women are prevalent in the following work classifications:</p>
     <!--INSERT TO SMALLER CARD-->
-    <h4 class="text-3xl py-4 font-medium">Primary occupation type</h4>
-        <p class="text-left">
-            Clerical Support Workers (p = 0.0245)<br>Service and Sales Workers (p = 0.0417)
-        </p>
-    <h4 class="text-3xl py-4 font-medium">Primary occupation industry type</h4>
-        <p class="text-left">
-            Wholesale and Retail Trade; Repair of Motor Vehicles* (p = 00014)
-            <br>Human Health and Social Work Activities (p = 0.0479)
-        </p>
-    <h4 class="text-3xl py-4 font-medium">Class of worker</h4>
-        <p class="text-left">
-            Private household (p = 0.0281)
-            <br>Without pay (Family-owned business) (p = 0.0005)**
-        </p>
+    <div class="max-w-4xl mx-auto bg-base-300 shadow-lg rounded-lg overflow-hidden m-5">
+          <!-- Smaller Card 1 -->
+          <div class="bg-base-100 rounded-lg p-4 mb-4">
+            <h4 class="text-3xl py-4 font-medium">Primary occupation type</h4>
+            <p class="text-left">
+              1. Clerical Support Workers (p = 0.0245)<br>
+              2. Service and Sales Workers (p = 0.0417)
+            </p>
+          </div>
+      
+          <!-- Smaller Card 2 -->
+          <div class="bg-base-100 rounded-lg p-4 mb-4">
+            <h4 class="text-3xl py-4 font-medium">Primary occupation industry type</h4>
+            <p class="text-left">
+              1. Wholesale and Retail Trade; Repair of Motor Vehicles* (p = 00014)<br>
+              2. Human Health and Social Work Activities (p = 0.0479)
+            </p>
+          </div>
+      
+          <!-- Smaller Card 3 -->
+          <div class="bg-base-100 rounded-lg p-4">
+            <h4 class="text-3xl py-4 font-medium">Class of worker</h4>
+            <p class="text-left">
+              1. Private household (p = 0.0281)<br>
+              2. Without pay (Family-owned business) (p = 0.0005)**
+            </p>
+          </div>
+      </div>
+      
 
     <p class="text-left">Meanwhile, women are significantly outnumbered in the following:</p>
-    <h4 class="text-3xl py-4 font-medium">Primary Occupation Type</h4>
-        <p class="text-left">
-            Skilled Agricultural, Forestry and Fishery Workers** (p = 0.0002)
-            <br>Craft and Related Trades Workers* (p = 0.0030)
-            <br>Plant and Machine Operators and Assemblers** (p = 0.0001)
-        </p>
-    <h4 class="text-3xl py-4 font-medium">Primary Occupation Industry Type</h4>
-        <p class="text-left">
-            Agriculture and Forestry** (p = 0.0004)
-            <br>Fishing and Aquaculture (p = 0.0580)
-            <br>Construction** (p = 0.0001)
-            <br>Transportation and Storage** (p = 0.0001)
-        </p>
-    <h4 class="text-3xl py-4 font-medium">Class of Worker</h4>
-        <p class="text-left">Private Establishment** (p = 0.0001)</p>
+
+    <div class="max-w-4xl mx-auto bg-base-300 shadow-lg rounded-lg overflow-hidden m-5">
+
+        <div class="bg-base-100 rounded-lg p-4 mb-4">
+            <h4 class="text-3xl py-4 font-medium">Primary Occupation Type</h4>
+            <p class="text-left">
+            1. Skilled Agricultural, Forestry and Fishery Workers** (p = 0.0002)<br>
+            2. Craft and Related Trades Workers* (p = 0.0030)<br>
+            3. Plant and Machine Operators and Assemblers** (p = 0.0001)
+            </p>
+        </div>
+
+        <div class="bg-base-100 rounded-lg p-4 mb-4">
+            <h4 class="text-3xl py-4 font-medium">Primary Occupation Industry Type</h4>
+            <p class="text-left">
+            1. Agriculture and Forestry** (p = 0.0004)<br>
+            2. Fishing and Aquaculture (p = 0.0580)<br>
+            3. Construction** (p = 0.0001)<br>
+            4. Transportation and Storage** (p = 0.0001)
+            </p>
+    </div>
+
+    <div class="bg-base-100 rounded-lg p-4">
+        <h4 class="text-3xl py-4 font-medium">Class of Worker</h4>
+            <p class="text-left">
+             1. Private Establishment** (p = 0.0001)
+            </p>
+    </div>
+</div>
     <!--END INSERT TO SMALLER CARD-->
     
     <h3 class="text-3xl py-4 font-medium">Identifying factors that correlate with gender gaps in the labor force</h3>
