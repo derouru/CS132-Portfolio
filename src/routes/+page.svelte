587d51f236a343d184b45728c29c6c4202f2d0a5
Derouru <derouru@gmail.com>
2024-06-19 17:59:21 +0800

    Added ascii flowers for team members

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -126,20 +126,51 @@
     <li>https://psada.psa.gov.ph/catalog/LFS/about</li>
   </div>
   <!-- ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ABOUT THE TEAM SECTION ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ -->
-  <footer class="footer p-10 bg-base-300 text-base-content">
-    <nav>
+  <footer class="footer p-10 bg-base-300 text-base-content flex justify-center">
+    <nav class="mx-12">
+      <pre>
+      .--.
+    .'_\/_'.
+    '. /\ .'
+      "|"
+       | /\
+    /\ |//\)
+   (/\\|/
+      \|
+      </pre>
       <h6 class="footer-title leading-3">Audrey Dela Vega</h6> 
       <a href="https://github.com/mycronare" class="link link-hover">@mycronare</a>
     </nav> 
-    <nav>
+    <nav class="mx-12">
+      <pre>
+           __/)
+        .-(__(=:
+     |\ |   \)
+     \ ||
+      \||
+       \|
+        |
+        |
+      </pre>
       <h6 class="footer-title leading-3">Janelle Mendoza</h6> 
       <a href="https://github.com/sojaja" class="link link-hover">@sojaja</a>
     </nav> 
-    <nav>
+    <nav class="mx-12">
+      <pre>
+      _
+    .\ /.
+   ( ~O~ )
+    '/_\'
+    \ | /
+     \|/
+      |
+      |
+      </pre>
       <h6 class="footer-title leading-3">Jackie Vicerra</h6> 
       <a href="https://github.com/derouru" class="link link-hover">@derouru</a>
     </nav>
   </footer>
+  
 </html>
 
 <style>
