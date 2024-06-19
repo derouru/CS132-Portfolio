36530f7de49b07eecb62bc1a106ead2cd8e600cb
Derouru <derouru@gmail.com>
2024-06-19 19:39:52 +0800

    Updated text chunks into components

---
+++ src/lib/Hypothesis/Hypothesis.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<html lang='en'>
+    <div class="flex flex-col h-3/4 w-3/4 mx-auto">
+        <div class="my-8 flex justify-center items-center">
+            <div class="max-w-4xl w-full p-6 bg-base-300 text-white rounded-lg shadow-lg">
+              <h3 class="text-3xl py-4 font-medium text-center text-fuchsia-300">TLDR; What's the problem?</h3>
+              <p>Gender bias strengthens the lack of institutionalized support for discrimination against women in the labor force.</p>
+            </div>
+          </div>
+          
+          <div class="my-8 flex justify-center items-center">
+            <div class="max-w-4xl w-full p-6 bg-base-300 text-white rounded-lg shadow-lg">
+                <h3 class="text-3xl py-4 font-medium text-center text-fuchsia-300">What's our objective?</h3>
+                <p>Use data science to study patterns and trends in the participation of women in the labor force, and propose solutions related to gender diversity and inclusion in the labor force.</p>
+            </div>
+        </div>
+    
+        <p class="my-4">
+          Clearly, the issue lies in pervasive, discriminatory gender norms, which are behind the gender gap in the labor force despite women being as competent and employable as men.
+          In an attempt to paint a better picture of how discrimination has affected opportunities for women to work, our group will investigate the gender gap present in the labor force.
+          </p>
+    
+        <p class="py-8 text-3xl font-medium text-center">Through data analysis, we aim to answer the following questions:</p> 
+        <p  class="text-3xl py-4 font-medium text-center ">Q1: What jobs under the labor force do women take the most?</p>
+        <p  class="text-3xl py-4 font-medium text-center ">Q2: Over the years, what factors correlate significantly with lack of women employment?</p>
+        <div class="divider"></div>
+        <div class="flex max-w-6xl justify-center mx-auto">
+          <div class="grid h-80 flex-grow card bg-base-300 rounded-box place-items-center">
+            <h3 class="text-3xl">Null Hypothesis</h3>
+            <p class="text-center text-fuchsia-300">There are no identifiable work types where women are more outnumbered. Additionally, there is no significant relationship between certain factors (e.g. region, relationship with house head, educational attainment, etc.) and the gender gap between men and women in the labor force.</p>
+          </div>
+          <div class="divider divider-horizontal"></div>
+          <div class="grid h-80 flex-grow card bg-base-300 rounded-box place-items-center">
+            <h3 class="text-3xl">Alternative Hypothesis</h3>
+            <p class="text-center p-5 text-fuchsia-300">There are job types with a more prevalent gender gap, where women are significantly outnumbered and less likely to be found participating in. There are also identifiable factors that closely correlate with the gap between men and women in the labor force.</p>
+          </div>
+        </div>
+        
+        <div class="divider"></div>
+        <h3 class="text-3xl py-4 font-medium text-center">So what now?</h3>
+          <p>We analyzed data from factors affecting bias in recruitment, opportunities, and participation of women in the labor force.</p>
+    </div>
+</html>
+
+<style>
+</style>
+
+
